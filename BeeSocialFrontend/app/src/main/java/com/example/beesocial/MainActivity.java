1fc59a9e05af5f6e62a3f7f5b7c39f5c9c6532dc
Alex Cepeda <alex.f.cepeda@gmail.com>
2019-09-18 22:49:40 -0400

    Added code to MainActivity.java to wire the button to the login page and LogInActivity.java to display the login page.

--- BeeSocialFrontend/app/src/main/java/com/example/beesocial/MainActivity.java
+++ BeeSocialFrontend/app/src/main/java/com/example/beesocial/MainActivity.java
@@ -14,12 +14,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Code that will switch the view to the registration page
         TextView registration = findViewById(R.id.registrationRedirect);
         registration.setMovementMethod(LinkMovementMethod.getInstance());
-        registration.setOnClickListener(new View.OnClickListener(){
+        registration.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, RegistrationActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        //Code that will switch the view to the login page
+        TextView login = findViewById(R.id.loginRedirect);
+        login.setMovementMethod(LinkMovementMethod.getInstance());
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, LogInActivity.class);
                 startActivity(intent);
             }
         });
