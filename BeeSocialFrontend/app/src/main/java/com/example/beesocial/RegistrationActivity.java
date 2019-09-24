60cffde12340f012e559dc5939bbe898df63957b
Alex Cepeda <alex.f.cepeda@gmail.com>
2019-09-23 21:59:17 -0400

    Finalized code that can send an email address and password to the database.

--- BeeSocialFrontend/app/src/main/java/com/example/beesocial/RegistrationActivity.java
+++ BeeSocialFrontend/app/src/main/java/com/example/beesocial/RegistrationActivity.java
@@ -1,96 +1,94 @@
 package com.example.beesocial;
 
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.DataOutputStream;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
 
-//Code adapted from https://www.tutlane.com/tutorial/android/android-login-and-registration-screen-design and
-//https://stackoverflow.com/questions/35390928/how-to-send-json-object-to-the-server-from-my-android-app
+
+/**
+ * Sources used:
+ * https://www.tutlane.com/tutorial/android/android-login-and-registration-screen-design
+ * https://stackoverflow.com/questions/35390928/how-to-send-json-object-to-the-server-from-my-android-app
+ * https://www.simplifiedcoding.net/android-volley-tutorial/
+ * https://www.kompulsa.com/how-to-send-a-post-request-in-android/
+ */
+
 public class RegistrationActivity extends AppCompatActivity {
+    EditText appFirstName;
+    EditText appLastName;
+    EditText appEmailAddress;
+    EditText appPassword;
+
     @Override
     protected void onCreate(Bundle savedInstance) {
         super.onCreate(savedInstance);
         setContentView(R.layout.registration_page);
 
-        Button registerButton = (Button) findViewById(R.id.registerButton);
-        final EditText firstName = (EditText) findViewById(R.id.firstName);
-        final EditText lastName = (EditText) findViewById(R.id.lastName);
-        final EditText emailAddress = (EditText) findViewById(R.id.emailAddress);
-        final EditText username = (EditText) findViewById(R.id.username);
-        final EditText password = (EditText) findViewById(R.id.password);
+        Button registerButton = findViewById(R.id.registerButton);
+        appFirstName = findViewById(R.id.firstName);
+        appLastName = findViewById(R.id.lastName);
+        appEmailAddress = findViewById(R.id.emailAddress);
+        appPassword = findViewById(R.id.password);
 
         registerButton.setOnClickListener(
                 new View.OnClickListener() {
-                    public void onClick(View view) {
-                        JSONObject userInfo = new JSONObject();
-                        try {
-                            userInfo.put("firstName", firstName.getText().toString());
-                            userInfo.put("lastName", lastName.getText().toString());
-                            userInfo.put("emailAddress", emailAddress.getText().toString());
-                            userInfo.put("username", username.getText().toString());
-                            userInfo.put("password", password.getText().toString());
-
-                            new SendDeviceDetails().execute("URL", userInfo.toString()); //Needs to be updated with server information
-                        } catch (JSONException e) {
-                            e.printStackTrace();
-                        }
+                    @Override
+                    public void onClick(View v) {
+                        registerUser();
                     }
                 }
         );
     }
 
-    private class SendDeviceDetails extends AsyncTask<String, Void, String> {
-        @Override
-        protected String doInBackground(String... params) {
-            String data = "";
-            HttpURLConnection httpURLConnection = null;
-            try {
-                httpURLConnection = (HttpURLConnection) new URL(params[0]).openConnection();
-                httpURLConnection.setRequestMethod("POST");
-                httpURLConnection.setDoOutput(true);
+    private void registerUser() {
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        String username = appEmailAddress.getText().toString().trim();
+        String password = appPassword.getText().toString().trim();
+        String url = "http://10.0.2.2:8888/api/users";
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject data = new JSONObject(response);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
 
-                DataOutputStream wr = new DataOutputStream(httpURLConnection.getOutputStream());
-                wr.writeBytes("PostData=" + params[1]);
-                wr.flush();
-                wr.close();
-
-                InputStream in = httpURLConnection.getInputStream();
-                InputStreamReader inputStreamReader = new InputStreamReader(in);
-
-                int inputStreamData = inputStreamReader.read();
-                while (inputStreamData != -1) {
-                    char current = (char) inputStreamData;
-                    inputStreamData = inputStreamReader.read();
-                    data += current;
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
-            } finally {
-                if (httpURLConnection != null) {
-                    httpURLConnection.disconnect();
-                }
+                    }
+                }) {
+            @Override
+            protected Map<String, String> getParams() {
+                Map<String, String> params = new HashMap<>();
+                params.put("username", username);
+                params.put("password", password);
+                return params;
             }
-            return data;
-        }
+        };
 
-        @Override
-        protected void onPostExecute(String result) {
-            super.onPostExecute(result);
-            Log.e("TAG", result);
-        }
+        requestQueue.add(stringRequest);
     }
+
+
 }
