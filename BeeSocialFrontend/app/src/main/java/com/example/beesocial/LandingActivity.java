a771b1da8d9d6bb12af7aaeac2455f0b690e9f85
Alex Cepeda <alex.f.cepeda@gmail.com>
2019-10-09 18:00:26 -0400

    Added a button and code to the landing page to redirect to the profile page. Added code to the profile activity class to display the profile page's XML. Added ProfileActivity to the Manifest.

--- BeeSocialFrontend/app/src/main/java/com/example/beesocial/LandingActivity.java
+++ BeeSocialFrontend/app/src/main/java/com/example/beesocial/LandingActivity.java
@@ -1,6 +1,10 @@
 package com.example.beesocial;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,5 +13,16 @@ public class LandingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstance) {
         super.onCreate(savedInstance);
         setContentView(R.layout.landing_page);
+
+        //Switches the view to the profile page once the button is clicked
+        TextView profile = findViewById(R.id.profileRedirect);
+        profile.setMovementMethod(LinkMovementMethod.getInstance());
+        profile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LandingActivity.this, ProfileActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- BeeSocialFrontend/app/src/main/java/com/example/beesocial/ProfileActivity.java
+++ BeeSocialFrontend/app/src/main/java/com/example/beesocial/ProfileActivity.java
@@ -1,10 +1,20 @@
 package com.example.beesocial;
 
+import android.os.Bundle;
 import android.widget.EditText;
 
-public class ProfileActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ProfileActivity extends AppCompatActivity {
     //Global variables for the text fields
     EditText appBirthDate;
     EditText genderIdentity;
     EditText favoriteFoods;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+    }
 }
