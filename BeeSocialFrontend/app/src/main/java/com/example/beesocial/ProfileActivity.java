1d9666511ffd4a19af72ad930f0a418fac559d7a
Alex Cepeda <alex.f.cepeda@gmail.com>
2019-10-10 14:40:26 -0400

    Finalized code to grab user profile information and save it to the backend.

--- BeeSocialFrontend/app/src/main/java/com/example/beesocial/ProfileActivity.java
+++ BeeSocialFrontend/app/src/main/java/com/example/beesocial/ProfileActivity.java
@@ -1,20 +1,32 @@
 package com.example.beesocial;
 
 import android.content.SharedPreferences;
+import android.os.Build;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.android.volley.Request;
 import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ProfileActivity extends AppCompatActivity {
     //Global variables for the text fields
@@ -33,8 +45,10 @@ public class ProfileActivity extends AppCompatActivity {
         appGenderIdentity = findViewById(R.id.genderIdentity);
         appFavoriteFoods = findViewById(R.id.favoriteFoods);
 
+        //Sets the behavior for when the button is clicked
         profileCreate.setOnClickListener(
                 new View.OnClickListener() {
+                    @RequiresApi(api = Build.VERSION_CODES.O)
                     @Override
                     public void onClick(View v) {
                         updateProfile();
@@ -43,23 +57,67 @@ public class ProfileActivity extends AppCompatActivity {
         );
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.O)
     private void updateProfile() {
+        //Creates the request queue
         RequestQueue requestQueue = Volley.newRequestQueue(this);
+
+        //Grabs the user information from the text fields
         String genderIdentity = appGenderIdentity.getText().toString().trim();
         String favoriteFoods = appFavoriteFoods.getText().toString().trim();
-//        Date userDate = null;
-//        SimpleDateFormat format = new SimpleDateFormat("MM/dd/yyyy");
-//        try {
-//            userDate = format.parse(appBirthDate.getText().toString().trim());
-//        } catch (ParseException e) {
-//            e.printStackTrace();
-//        }
 
-        //get the userID from shared preferences
+        //Grabs the user birthdate and calculates the difference between then and now
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM/dd/yyyy");
+        LocalDate birthdate = LocalDate.parse(appBirthDate.getText().toString().trim(), formatter);
+        LocalDate currentDate = LocalDate.now();
+        Long years = ChronoUnit.YEARS.between(birthdate, currentDate);
+        String age = Long.toString(years);
+
+        //Gets the user ID from Shared Preferences and creates a path to update
         SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
         String ID = sharedPreferences.getString("id", "");
         String url = "http://10.0.2.2:8888/api/profile/" + ID; //URL where the information will be sent
-        System.out.println(url);
 
+        StringRequest stringRequest = new StringRequest(Request.Method.PUT, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        Toast toast = Toast.makeText(getApplicationContext(),
+                                "Profile updated!",
+                                Toast.LENGTH_LONG);
+                        toast.show();
+                    }
+                },
+
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                    }
+                }) {
+
+            //Creates a header with the authentication token
+            @Override
+            public Map<String, String> getHeaders() {
+                Map<String, String> headers = new HashMap<>();
+                SharedPreferences sharedPreferences =
+                        PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+                String token = sharedPreferences.getString("token", "");
+                System.out.println(token);
+                headers.put("Authorization", "Bearer " + token);
+                return headers;
+            }
+
+            //Creates the parameters for the request body
+            @Override
+            protected Map<String, String> getParams() {
+                Map<String, String> params = new HashMap<>();
+                params.put("age", age);
+                params.put("sex", genderIdentity);
+                params.put("description", favoriteFoods);
+                return params;
+            }
+        };
+        requestQueue.add(stringRequest);
     }
 }
