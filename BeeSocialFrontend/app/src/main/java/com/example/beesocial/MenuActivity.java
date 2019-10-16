103ca498de9030f2835fc67b02abf68df5e71a9a
Christy Tropila <ctropilalakebeverage@gmail.com>
2019-10-16 11:44:51 -0400

    fixed navigation between pages

--- BeeSocialFrontend/app/src/main/java/com/example/beesocial/MenuActivity.java
+++ BeeSocialFrontend/app/src/main/java/com/example/beesocial/MenuActivity.java
@@ -1,5 +1,6 @@
 package com.example.beesocial;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -8,6 +9,7 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.text.method.LinkMovementMethod;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -19,6 +21,8 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -30,31 +34,101 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
 
 
-public class MenuActivity extends AppCompatActivity {
+public class MenuActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     DrawerLayout drawerLayout;
     ActionBarDrawerToggle drawerToggle;
     Toolbar toolbar;
+    NavigationView navigationView;
 
 
     @Override
-    protected void onCreate (Bundle savedInstanceState){
+    protected void onCreate (Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
-        toolbar= findViewById(R.id.toolbar);
+        toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
 
-
-            //drawer menu settings
-        drawerLayout= findViewById(R.id.drawer);
-        drawerToggle= new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open_drawer, R.string.close_drawer);
+        //drawer menu settings
+        drawerLayout = findViewById(R.id.drawer);
+        drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open_drawer, R.string.close_drawer);
         drawerLayout.addDrawerListener(drawerToggle);
         drawerToggle.setDrawerIndicatorEnabled(true);
         drawerToggle.syncState();
+
+        if(savedInstanceState==null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFrag()).commit();
+        }
+
+        navigationView = findViewById(R.id.navigationView);
+        navigationView.setNavigationItemSelectedListener(this);
+
     }
+
+    //switches between the different pages on the map
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        switch(menuItem.getItemId()){
+            case R.id.home:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFrag()).commit();
+                break;
+            case R.id.createevent:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new CreateEventFrag()).commit();
+                break;
+            case R.id.editevent:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new EditEventFrag()).commit();
+                break;
+            case R.id.chats:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new ChatFrag()).commit();
+                break;
+            case R.id.settings:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new SettingsFrag()).commit();
+                break;
+            case R.id.logout:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new LogoutFrag()).commit();
+                break;
+        }
+
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+
+
+  /*  @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+      //  Fragment fragmentClass=null;
+        switch(menuItem.getItemId()){
+            case R.id.home:
+                Toast.makeText(this,"Home Btn Clicked", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.createevent:
+                Toast.makeText(this,"Create Event Clicked", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.editevent:
+                Toast.makeText(this,"Edit Event Clicked", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.chats:
+                Toast.makeText(this,"Chats Clicked", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.settings:
+                Toast.makeText(this,"Settings Clicked", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.logout:
+                Toast.makeText(this,"Logout Clicked", Toast.LENGTH_SHORT).show();
+                break;
+
+        }
+        return false;
+    }
+*/
+
 
 }
