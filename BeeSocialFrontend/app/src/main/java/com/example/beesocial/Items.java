eca70f2e30cdebaf227b3c94e81db692dbaa3c95
Christy Tropila <ctropilalakebeverage@gmail.com>
2019-11-07 18:25:03 -0500

    worked on front xml for event status cards. not able to connect json

---
+++ BeeSocialFrontend/app/src/main/java/com/example/beesocial/Items.java
@@ -0,0 +1,56 @@
+package com.example.beesocial;
+
+public class Items {
+
+
+    //this is to get the event objects loaded onto the cards on event status fragment
+
+
+    private String title;
+    private String location;
+    private String date;
+    private String time;
+
+    public Items(){
+
+    }
+
+    public Items(String title, String location, String date, String time) {
+        this.title = title;
+        this.location = location;
+        this.date = date;
+        this.time = time;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+}
